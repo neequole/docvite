1e136b10f3a08604aea271b9926de90dce135737
neequole <nicole.tibay@johngaltventures.com>
2017-12-07 17:10:39 +0800

    Add id to serializers

--- backoffice/api/serializers.py
+++ backoffice/api/serializers.py
@@ -6,16 +6,16 @@ from ..models import Client, Doctor, Invitation
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ('first_name', 'last_name', 'referral_code', 'username')
+        fields = ('id', 'first_name', 'last_name', 'referral_code', 'username')
 
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email')
 
 
 class InvitationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invitation
-        fields = ('doctor', 'client', 'created', 'updated', 'status', 'is_sent')
+        fields = ('id', 'doctor', 'client', 'created', 'updated', 'status', 'is_sent')
